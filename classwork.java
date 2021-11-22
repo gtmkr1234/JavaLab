cf38bb96eeb0ab23c00c4cdb4f288fc0e6240f78
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2021-11-22 21:38:48 +0530

    Create classwork.java

---
+++ classwork.java
@@ -0,0 +1,12 @@
+package com.company;
+class programming{
+    programming(String a){
+        System.out.println("I love "+a);
+    }
+}
+public class class_work_26 {
+    public static void main(String[] args) {
+        String a="Krishna";
+        programming m=new programming(a);
+    }
+}
