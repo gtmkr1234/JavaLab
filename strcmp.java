6d2c10b11e2476799b42e6a75ae2b770987ac725
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2021-11-26 00:16:26 +0530

    String comparision that were initialised by different methods.

---
+++ strcmp.java
@@ -0,0 +1,8 @@
+public class test5 {
+    public static void main(String[] args) {
+        String s1 = "GLA";
+        String s2 = new String("GLA");
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+    }
+}
