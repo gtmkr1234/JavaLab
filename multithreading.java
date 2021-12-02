d38cf9326387abcb57cd7a7ef8d99751fb5d4231
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2021-12-02 13:17:40 +0530

    Multithreading introduction

---
+++ multithreading.java
@@ -0,0 +1,18 @@
+class Thread1 extends Thread {
+    public void run(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Thread 1");
+        }
+        System.out.println(10/0);
+    }
+}
+class Thread2 extends Thread {
+    public void run(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Thread 2");
+        }
+        System.out.println();
+    }
+}
+public class Problem1 {
+}
